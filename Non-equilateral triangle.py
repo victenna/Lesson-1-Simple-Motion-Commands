76a4859979e84852b38ec782455e06fb07d1f68c
victenna <39585970+victenna@users.noreply.github.com>
2019-11-05 14:35:21 -0500

    Add files via upload

---
+++ Non-equilateral triangle.py
@@ -0,0 +1,13 @@
+import turtle
+t=turtle.Turtle()
+turtle.bgcolor("violet")        
+t.color("blue")
+t.pensize(5)                
+
+t.forward(400)
+t.left(120)
+t.forward(220)
+t.goto(0,0)
+
+
+canvas.exitonclick()            # wait for a user click on the canvas
