6ac17ca116a711227344d0a62ece4f6af3733f98
victenna <39585970+victenna@users.noreply.github.com>
2019-11-05 14:40:56 -0500

    Add files via upload

---
+++ Parallelogram.py
@@ -0,0 +1,15 @@
+import turtle
+t=turtle.Turtle()
+turtle.bgcolor("violet")        
+t.color("blue")
+t.pensize(5)                
+
+t.forward(400)
+t.rt(140)
+t.forward(260)
+t.rt(40)
+t.fd(400)
+t.rt(140)
+t.fd(260)
+t.hideturtle()
+turtle.exitonclick()            # wait for a user click on the canvas
